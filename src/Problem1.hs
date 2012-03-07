5de59cbb57e0ce05108854b1016ebac416caa135
Daryl Dudey <daryljdudey@googlemail.com>
2012-03-07 22:43:58 +0000

    Solved problem 1

--- src/Problem1.hs
+++ src/Problem1.hs
@@ -1,2 +1,8 @@
+import Shared
+
 main = do
-	print "Done"
+  let xs3 = [i | i <- [3,6..999], rem i 5 /= 0]
+      xs5 = [i | i <- [5,10..999], rem i 3 /= 0]
+      xs35 = [i | i <- [1..999], rem i 3 == 0 && rem i 5 == 0]
+      xs = xs3 ++ xs5 ++ xs35
+  print $ foldl (+) 0 xs
