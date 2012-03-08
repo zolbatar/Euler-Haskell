0f268fedfba00fca8a793993d595c69e5a431fb8
Daryl Dudey <daryljdudey@googlemail.com>
2012-03-08 23:00:46 +0000

    Still not done problem 3, but getting closer now

--- src/Problem2.hs
+++ src/Problem2.hs
@@ -8,6 +8,7 @@ fib1 :: [Int] -> Int -> [Int]
 fib1 [] q = fib1 [2,1] q
 fib1 (x:y:xs) q = if (x < q) then fib1 ([x+y,x,y] ++ xs) q else [y] ++ xs
 
+-- No good as we need to do an even check
 --fib2 0 = 0
 --fib2 1 = 1
 --fib2 n = fib2 (n-1) + fib2 (n-2) 
--- src/Problem3.hs
+++ src/Problem3.hs
@@ -1,10 +1,21 @@
-main = do
-  print $ primes 100
-  print $ primes 13195
-  print $ primes 600851475143
+import Data.Int
+import Data.List
 
-primes x = filter (\y -> rem x y == 0) xs
-	where xs = [2..(x-1)]
+main = do
+  print $ primefactors 100 []
+  print $ primefactors 13195 []
+  print $ primefactors 600851475143 []
+
+primefactors :: Int64 -> [Int64]-> [Int64]
+primefactors x xs = if valid then primefactors (quot x firstdivided) (xs ++ [firstdivided]) else (xs ++ [x])
+  where div = [2..(quot x 2)]
+        divided = filter (\y -> rem x y == 0) div
+        firstdivided = head divided
+        valid = length divided > 0
+
+--primes :: Int64 -> [Int64]
+--primes x = filter (\y -> rem x y == 0) xs
+--  where xs = [2..(quot x 2)]
 
 --primes y = primes' 2 y []
 
