a9c56760c621d71e39a6f730e5b837fff1f7e633
Daryl Dudey <daryl.dudey@360scheduling.com>
2012-03-09 12:05:52 +0000

    Nearly there surely now

--- src/Problem3.hs
+++ src/Problem3.hs
@@ -2,26 +2,40 @@ import Data.Int
 import Data.List
 
 main = do
-  print $ primefactors 100 []
-  print $ primefactors 13195 []
-  print $ primefactors 600851475143 []
+  a <- primefactors 13195 pl []
+  print a
+  b <- primefactors 600851475143 pl []
+  print b
+--  print $ primefactors 13195 pl []
+--  print $ primefactors 600851475143 pl []
+  where pl = primesList
 
-primefactors :: Int64 -> [Int64]-> [Int64]
-primefactors x xs = if valid then primefactors (quot x firstdivided) (xs ++ [firstdivided]) else (xs ++ [x])
+primefactors :: Integer -> [Integer]-> [Integer] -> IO [Integer]
+primefactors x pl xs = do
+  print x
+  print complete
+--  print divided
+  print xs
+  print (complete * divided)
+  print ""
+  if (complete * divided) == x then return (xs ++ [x]) else primefactors (quot x divided) pl (xs ++ [divided])
   where div = [2..(quot x 2)]
-        divided = filter (\y -> rem x y == 0) div
-        firstdivided = head divided
-        valid = length divided > 0
+        divided = head $ filter (\y -> rem x y == 0) div
+        complete = foldl (*) 1 xs
 
---primes :: Int64 -> [Int64]
---primes x = filter (\y -> rem x y == 0) xs
---  where xs = [2..(quot x 2)]
+primesList :: [Integer]
+primesList = primes 500 -- 775146
 
---primes y = primes' 2 y []
+primes y = primes' 2 y []
 
---primes' :: Int -> Int -> [Int] -> [Int]
---primes' x z xs = 
---	if x == z then [1,2] ++ xs
---  else if isprime then primes' (x+1) z (xs ++ [x]) else primes' (x+1) z xs
---	where l1 = filter (\y -> (rem x y == 0)) xs
---	      isprime = length l1 == 0
+primes' :: Integer -> Integer -> [Integer] -> [Integer]
+primes' x z xs =
+  if x >= z then xs
+  else if isprime then primes' (x+1) z (xs ++ [x]) else primes' (x+1) z xs
+	where sr = sqr x
+	      xss = filter (\y -> y <= sr) xs
+	      l1 = filter (\y -> (rem x y == 0)) xss
+	      isprime = length l1 == 0
+
+sqr :: Integer -> Integer
+sqr x = toInteger $ ceiling $ sqrt (fromIntegral x)
