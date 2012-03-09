6c7a81099c33dabd160ca9b8f5841748da1f55d6
Daryl Dudey <daryl.dudey@360scheduling.com>
2012-03-09 15:30:12 +0000

    Solved problem 4

--- src/Problem4.hs
+++ src/Problem4.hs
@@ -1,3 +1,16 @@
+import Data.List
+
 main = do
-  print xs
-  where xs = [(i,j) | i <- [1..99], j <- [1..99]]
+  print xs4
+  where xs = [(i,j) | i <- [1..999], j <- [1..999]] -- Build all combinations
+        xs2 = map (\(x,y) -> (x,y,x * y)) xs-- Now build the multiplied number
+        xs3 = filter (\(x,y,z) -> isPalindrome $ show z) xs2
+        xs4 = sort $ map (\(x,y,z) -> z) xs3 
+
+isPalindrome :: String -> Bool
+isPalindrome x = 
+  start == end
+  where len = div (length x) 2
+        start = take len x
+        end = take len $ reverse x
+  
